2d88c7239c57c39fb5994f87a7a2bbfe890a28c4
Mehnaz001 <mehnaz62844@gmail.com>
2026-09-16 07:52:21 +0530

    day-9 loops done

---
+++ Day9.js
@@ -0,0 +1,116 @@
+// //Loops
+
+// //For loop
+// // for(initialisation, condition, updation) {
+// //     // code to execute
+// // }
+
+// for(let i=1; i<=5; i++) {
+//     console.log(i)
+// }
+
+// //Table print
+// for(let i=1; i<=10; i++) {
+//     console.log(2*i)
+// }
+
+// //practice ques - print odd or even 1-10
+// for(let i=1; i<=10; i++) {
+//     if(i%2==0) {
+//         console.log(`${i} is even`)
+//     } else {
+//        console.log(`${i} is odd`) 
+//     }
+// }
+
+// //nested for loops
+// for(let i=1; i<=3; i++) {
+//     for(let j=1; j<=3; j++) {
+//         console.log(j)
+//     }
+// }
+
+// //Practice ques
+
+// // *
+// // **
+// // ***
+// // ****
+// // *****
+
+// for(let row=1; row<=5; row++) {
+//     let pattern = ""
+//     for(let col=1; col<=row; col++) {
+//         pattern = pattern + "*"
+//     }
+//     console.log(pattern)
+// }
+
+// // 1
+// // 12
+// // 123
+// // 1234
+// // 12345
+
+// for(let i=1; i<=5; i++) {
+//     let pattern = ""
+//     for(let j=1; j<=i; j++) {
+//         pattern = pattern + j
+//     }
+//     console.log(pattern)
+// }
+
+// //while loop
+
+// // while(condition) {
+// //     //do something
+// //     update
+// // }
+
+// let num = 1;
+
+// while(num<=10) {
+//     console.log(num)
+//     num++
+// }
+
+// let number = 1;
+
+// while(number<=10) {
+//     if(number==5) {
+//         console.log("Special number")
+//         break;
+//     }
+//     console.log(number)
+//     number++;
+// }
+
+//Count digit
+// let num = 12345
+// let count = 0;
+// while(num>0) {
+//     num = Math.floor(num/10)
+//     count++;
+// }
+// console.log(count)
+
+//Revrese
+num = 12345
+// let reverse = 0
+
+// while(num>0) {
+//     let digit = Math.floor(num%10)
+//     reverse = reverse*10 + digit
+//     num = Math.floor(num/10)
+// }
+
+// console.log(reverse)
+
+//5*10 = 50+4 = 54
+
+// let ans = 0
+// while(num>0) {
+//     let digit = Math.floor(num%10)
+//     ans = 
+// }
+
