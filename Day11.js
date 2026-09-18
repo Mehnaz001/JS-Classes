d1ef9f3400d23994e97949cfe73b7ab8255ed2fc
Mehnaz001 <mehnaz62844@gmail.com>
2026-09-18 07:57:26 +0530

    loops question practice

---
+++ Day11.js
@@ -0,0 +1,30 @@
+//Loops practice question
+
+//Swap number
+// let a = 10
+// let b = 20
+// let temp = b
+//     b = a
+//     a = temp
+
+//Reverse a string
+// let str = "Mehnaz"
+// let reverse = ""
+// for(let i=str.length-1; i>=0; i--) {
+//     reverse = reverse + str[i]
+// }
+// console.log(reverse)
+
+// let newStr = str.split("").reverse().join("")
+// console.log(newStr)
+
+//Check whether a string is palindrome or not
+let str = "naman"
+let reverse = str.split("").reverse().join("")
+if(str == reverse) {
+    console.log("yes its a valid palindrome")
+}
+
+//Reverse a number
+let number = 1221
+//Check wheter a number is palindrome or not
